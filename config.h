709909a13014c6fc51d5dcf68345caec0e0d1c96
oskar paulsson <oskar@paulsson.dev>
2022-07-06 08:32:18 +0200

    add centered master patch

--- config.h
+++ config.h
@@ -39,9 +39,9 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
+	{ "|M|",      centeredmaster },    /* first entry is default */
+	{ "[]=",      tile },
+	{ ">M>",      centeredfloatingmaster },	
 };
 
 /* key definitions */
@@ -74,9 +74,9 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY|ShiftMask,             XK_y,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY|ShiftMask,             XK_u,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY|ShiftMask,             XK_i,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
